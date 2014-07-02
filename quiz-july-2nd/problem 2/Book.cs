9ddf6613a2d73f553515bbca586c1396637eda34
Lionell Brown <ltrainb@gmail.com>
2014-07-02 16:20:45 -0400

    project 2 and 3
    done

---
+++ quiz-july-2nd/problem 2/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_2
+{
+    class Book
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+
+        public Book(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
---
+++ quiz-july-2nd/problem 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_2
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            int value;
+            Book comicBook = new Book("Marvel vs DC", 21.99);
+            Book noval = new Book("Deception Point", 25.00);
+            value=compareBooks(comicBook, noval);
+            Console.WriteLine(value);
+            Console.ReadKey();
+
+
+        }
+        public static int compareBooks( Book book1,  Book  book2)
+        {
+            int value;
+            if (book1.price == book2.price)
+                value= 0;
+            else if (book1.price > book2.price)
+                value= 1;
+            else
+                value=-1;
+            return value;
+
+        }
+    }
+}
